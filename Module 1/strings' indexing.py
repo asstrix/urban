13950d61c13d1bf6ee6564187fb21e7bff769111
Danila Martynov <danil@cyp.mq>
2024-04-08 11:14:49 +0300

    Initial commit

---
+++ Module 1/strings' indexing.py
@@ -0,0 +1,7 @@
+string = 'это моя строка'
+print(string[0])                        # 1
+print(string[-1])                       # 2
+print(string[2:6])                      # 3
+print(string[::-1])                     # 4
+print(len(string))                      # 5
+print('это новая строка' + string)      # 6
