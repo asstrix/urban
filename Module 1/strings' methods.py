eb79f5194f99130218e29410ac4466676f110b2b
danil <asstrix@protonmail.com>
2024-04-09 10:59:00 +0300

    Done

---
+++ Module 1/strings' methods.py
@@ -0,0 +1,7 @@
+myString = input('Write something here: ')      # Создайте переменную и присвойте ей значение строки с произвольным текстом (функция input()).
+print('Input length:', len(myString))           # Выведите на экран длину строки.
+print(myString.upper())                         # Выведите строку в верхнем регистре.
+print(myString.lower())                         # Выведите строку в нижнем регистре.
+print(myString.replace(' ', ''))    # Измените строку, удалив все пробелы.
+print(myString[0])                              # Выведите первый символ строки.
+print(myString[-1])                             # Выведите последний символ строки.
