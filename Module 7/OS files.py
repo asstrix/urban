2fcd464f05b6743df6620e69b54c0e9e6941b6b2
danil <asstrix@protonmail.com>
2024-05-30 15:32:58 +0300

    done

--- Module 7/OS files.py
+++ Module 7/OS files.py
@@ -3,8 +3,8 @@ import os, time
 directory = f"C:\\Users\\{os.getlogin()}\\Downloads\\"
 for i, j, k in os.walk(directory):
 	for file in k:
-		fullpath = os.path.join(i, file)
-		size = os.path.getsize(fullpath)
-		htime = time.strftime("%H:%M:%S %d.%m.%Y", time.localtime(os.path.getmtime(fullpath)))
-		dirname = os.path.dirname(i)
-		print(f'file name: {fullpath}, dir_name: {dirname}, size: {size / 1024:.2f} Kb, time: {htime}')
+		path = os.path.join(i, file)
+		size = os.path.getsize(path)
+		htime = time.strftime("%H:%M:%S %d.%m.%Y", time.localtime(os.path.getmtime(path)))
+		dirname = os.path.dirname(path)
+		print(f'file name: {path}, dir_name: {dirname}, size: {size / 1024:.2f} Kb, modified time: {htime}')
