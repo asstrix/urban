f768eec9e76fa0af8cea8bbe4cac4553931cd4d4
asstrix <65392270+asstrix@users.noreply.github.com>
2024-06-17 13:01:16 +0300

    Update Decorators.py

--- Module 9/Decorators.py
+++ Module 9/Decorators.py
@@ -5,16 +5,16 @@ def is_prime(func):
     def wrapper(*args):
         res = func(*args)
         if res <= 1:
-            print("Neither Composite nor Simple")
+            print('Neither Composite nor Simple')
         elif res == 2:
             print("Simple")
         else:
             for i in range(2, int(math.sqrt(res)) + 1):
                 if res % i == 0:
-                    print("Composite")
+                    print('Composite')
                     break
             else:
-                print("Simple")
+                print('Simple')
         return res
     return wrapper
 
