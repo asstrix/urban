568cafa96538844837edd82398d655dfd326a263
Danil <asstrix@protonmail.com>
2024-07-19 20:55:49 +0300

    Done

---
+++ Module 13/module_13_3.py
@@ -0,0 +1,21 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+
+api = '7490221133:AAG4JGi-WGKsb_4x2wXjAjLRdz08OBltTMY'
+
+bot = Bot(token=api)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def any_message(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
