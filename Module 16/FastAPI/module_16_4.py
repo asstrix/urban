072a7030e08e90b21d7c4d5468028d0289a5f6e2
danil <asstrix@protonmail.com>
2024-08-05 17:33:30 +0300

    Done

---
+++ Module 16/FastAPI/module_16_4.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI, HTTPException, Path
+from pydantic import BaseModel
+from typing import List
+app = FastAPI()
+
+users = []
+
+
+class User(BaseModel):
+    id: int = None
+    username: str
+    age: int = None
+
+
+@app.get('/users')
+async def get_users() -> List[User]:
+    return users
+
+
+@app.post('/user/{username}/{age}')
+# async def add_user(username: str = Path(min_length=5, max_length=20, description='Enter username'),
+# 				   age: int = Path(ge=18, le=120, description='Enter age')) -> str:
+async def add_user(user: User):
+    if not len(users):
+        User.id = 1
+    else:
+        User.id = str(len(users) + 1)
+    users.append(user)
+    return f'User <{User.id}> has been registered'
+
+
+@app.put('/user/{user_id}/{username}/{age}')
+def update_user(user: User) -> str:
+    try:
+        users[user.id - 1] = user
+        return f'The user <{user.id}> has been updated'
+    except IndexError:
+        raise HTTPException(status_code=404, detail="Message not found")
+
+
+@app.delete('/user/{user_id}')
+async def delete_user(user_id: int = Path(ge=1, le=100)):
+    users.pop(user_id - 1)
+    return f'The user <{user_id}> has been deleted'
