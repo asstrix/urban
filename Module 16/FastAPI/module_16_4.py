83be2ec8043007f9fd71b32476efc1659bb645dd
danil <asstrix@protonmail.com>
2024-08-05 17:40:25 +0300

    Done

--- Module 16/FastAPI/module_16_4.py
+++ Module 16/FastAPI/module_16_4.py
@@ -18,21 +18,19 @@ async def get_users() -> List[User]:
 
 
 @app.post('/user/{username}/{age}')
-# async def add_user(username: str = Path(min_length=5, max_length=20, description='Enter username'),
-# 				   age: int = Path(ge=18, le=120, description='Enter age')) -> str:
 async def add_user(user: User):
     if not len(users):
         User.id = 1
     else:
         User.id = str(len(users) + 1)
     users.append(user)
-    return f'User <{User.id}> has been registered'
+    return f'User <{user.id}> has been registered'
 
 
 @app.put('/user/{user_id}/{username}/{age}')
 def update_user(user: User) -> str:
     try:
-        users[user.id - 1] = user
+        users[user.id] = user
         return f'The user <{user.id}> has been updated'
     except IndexError:
         raise HTTPException(status_code=404, detail="Message not found")
@@ -41,7 +39,7 @@ def update_user(user: User) -> str:
 @app.delete('/user/{user_id}')
 async def delete_user(user_id: int = Path(ge=1, le=100)):
     try:
-        users.pop(user_id - 1)
+        users.pop(user_id)
         return f'The user <{user_id}> has been deleted'
     except IndexError:
         raise HTTPException(status_code=404, detail="User was not found")
