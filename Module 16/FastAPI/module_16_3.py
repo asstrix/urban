77248d908bb92cd5a20244ba0dc892aaf1c96259
danil <asstrix@protonmail.com>
2024-08-05 15:46:42 +0300

    Done

--- Module 16/FastAPI/module_16_3.py
+++ Module 16/FastAPI/module_16_3.py
@@ -21,12 +21,12 @@ async def add_user(username: str = Path(min_length=5, max_length=20, description
 @app.put('/user/{user_id}/{username}/{age}')
 async def update_user(user_id: str = Path(min_length=1, max_length=100, description='Enter user id'),
 					  username: str = Path(min_length=5, max_length=20, description='Enter username'),
-					  age: int = Path(ge=18, le=120, description='Enter age')):
+					  age: int = Path(ge=18, le=120, description='Enter age')) -> str:
 	users[user_id] = f'Name: {username}, Age: {age}'
 	return f'The user <{user_id}> has been updated'
 
 
 @app.delete('/user/{user_id}')
-async def delete_user(user_id: str = Path(min_length=1, max_length=100)):
+async def delete_user(user_id: str = Path(min_length=1, max_length=100)) -> str:
 	users.pop(user_id)
 	return f'The user <{user_id}> has been deleted'
