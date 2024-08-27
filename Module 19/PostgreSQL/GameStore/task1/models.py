b47349628bd9fc170f251339ec877253d018ed32
danil <asstrix@protonmail.com>
2024-08-27 17:08:22 +0300

    First commit

---
+++ Module 19/PostgreSQL/GameStore/task1/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Buyer(models.Model):
+	name = models.CharField(max_length=32)
+	balance = models.DecimalField(max_digits=10, decimal_places=2)
+	age = models.DecimalField(max_digits=3, decimal_places=0)
+
+	def __str__(self):
+		return self.name
+
+
+class Game(models.Model):
+	title = models.CharField(max_length=32)
+	cost = models.DecimalField(max_digits=10, decimal_places=2)
+	size = models.DecimalField(max_digits=10, decimal_places=2)
+	description = models.CharField(max_length=256)
+	age_limited = models.BooleanField(default=False)
+	buyer = models.ManyToManyField(Buyer)
+
+	def __str__(self):
+		return self.title
