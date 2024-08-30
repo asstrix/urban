210566041aa5183ce8d477c0458c49be163288a2
danil <asstrix@protonmail.com>
2024-08-30 12:42:54 +0300

    First commit

--- Module 19/19.1/GameStore/task1/models.py
+++ Module 19/19.1/GameStore/task1/models.py
@@ -18,7 +18,7 @@ class Game(models.Model):
 	size = models.DecimalField(max_digits=10, decimal_places=2)
 	description = models.CharField(max_length=256)
 	age_limited = models.BooleanField(default=False)
-	buyer = models.ManyToManyField(Buyer)
+	buyer = models.ManyToManyField(Buyer, related_name='games', default=None)
 
 	def __str__(self):
 		return self.title
