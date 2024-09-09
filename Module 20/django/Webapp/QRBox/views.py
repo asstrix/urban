5f5e6d0172920d45b914141b53d59f208a3583a2
Danil <asstrix@protonmail.com>
2024-09-09 21:36:54 +0300

    Added reg form

--- Module 20/django/Webapp/QRBox/views.py
+++ Module 20/django/Webapp/QRBox/views.py
@@ -1,9 +1,44 @@
-from django.shortcuts import render
 from django.contrib.auth.views import LoginView
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(required=True, label=None)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super(SignUpForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+
+
+def reg_page(request):
+    title = 'QRBox: Register'
+    context = {
+        'title': title,
+    }
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = SignUpForm()
+    return render(request, 'register.html', {'form': form, 'context': context})
 
 
 class CustomLoginView(LoginView):
-    template_name = 'login.html'  # путь к вашему шаблону
+    template_name = 'login.html'
 
 
 def main_page(request):
@@ -20,12 +55,4 @@ def login_page(request):
         'title': title,
     }
     return render(request, 'login.html', context)
-
-
-def reg_page(request):
-    title = 'QRBox: Register'
-    context = {
-        'title': title,
-    }
-    return render(request, 'register.html', context)
 
