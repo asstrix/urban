c690d9a8357b1e73917277edef49da793e69bbd1
Danil <asstrix@protonmail.com>
2024-10-02 21:51:18 +0300

    mixed all files in one dataframe

--- priceanalyser/project.py
+++ priceanalyser/project.py
@@ -5,37 +5,83 @@ import pandas as pd
 class PriceMachine:
     
     def __init__(self):
-        self.data = {}
+        self.data = []
         self.result = ''
         self.name_length = 0
-    
+
+    # def load_prices(self, file_path=''):
+    #     keyword = 'price'
+    #     required_columns = [['наименование','товар', 'название', 'продукт'],
+    #                         ['цена','розница'],
+    #                         ['вес', 'масса', 'фасовка']]
+    #     path = Path(file_path)
+    #     first_file = True
+    #     for file in path.rglob('*'):
+    #         if keyword in file.name:
+    #             try:
+    #                 if first_file:
+    #                     df = pd.read_csv(file)
+    #                     first_file = False
+    #                 else:
+    #                     df = pd.read_csv(file, header=None, skiprows=1)
+    #                 rubbish = [i for i in df.columns if i not in [j for k in required_columns for j in k]]
+    #                 df = df.drop(columns=rubbish).dropna(axis=1, how='all')
+    #                 rename_map = {}
+    #                 missing_columns = []
+    #                 for i in required_columns:
+    #                     found_column = next((col for col in df.columns if col in i), None)
+    #                     if found_column:
+    #                         standard_name = i[0]
+    #                         rename_map[found_column] = standard_name
+    #                     else:
+    #                         missing_columns.append(i)
+    #                 if not missing_columns:
+    #                     df = df.rename(columns=rename_map)
+    #                     df = df[[i[0] for i in required_columns]]
+    #                     df['файл'] = file.name
+    #                     df['цена за кг.'] = round(df['цена']/df['вес'], 1)
+    #                     self.data.append(df)
+    #                 if self.data:
+    #                     self.data = pd.concat(self.data, ignore_index=True)
+    #             except Exception as e:
+    #                 print(f"Error processing file {file}: {e}")
+    #     return self.data
+
     def load_prices(self, file_path=''):
         keyword = 'price'
-        required_columns = [['наименование','товар', 'название', 'продукт'],
-                            ['цена','розница'],
-                            ['вес', 'масса', 'фасовка']]
+        required_columns = [
+            ['наименование', 'товар', 'название', 'продукт'],
+            ['цена', 'розница'],
+            ['вес', 'масса', 'фасовка']
+        ]
+
         path = Path(file_path)
+        first_file = True
         for file in path.rglob('*'):
             if keyword in file.name:
                 try:
                     df = pd.read_csv(file)
-                    rubbish = [i for i in df.columns if i not in [j for k in required_columns for j in k]]
-                    df = df.drop(columns=rubbish).dropna(axis=1, how='all')
+                    valid_columns = [col for sublist in required_columns for col in sublist]
+                    df = df[[col for col in df.columns if col in valid_columns]].dropna(axis=1, how='all')
                     rename_map = {}
-                    missing_columns = []
                     for i in required_columns:
-                        found_column = next((col for col in df.columns if col in i), None)
-                        if found_column:
-                            standard_name = i[0]
-                            rename_map[found_column] = standard_name
-                        else:
-                            missing_columns.append(i)
-                    if not missing_columns:
-                        df = df.rename(columns=rename_map)
-                        df = df[[i[0] for i in required_columns]]
-                        df['файл'] = file.name
-                        df['цена за кг.'] = round(df['цена']/df['вес'], 1)
-                        self.data[file.name] = df
+                        column_found = next((col for col in df.columns if col in i), None)
+                        if column_found:
+                            rename_map[column_found] = i[0]
+                    df = df.rename(columns=rename_map)
+                    if not all([i[0] in df.columns for i in required_columns]):
+                        print(f"File {file} skipped: required columns are absent")
+                        continue
+                    correct_order = [col_group[0] for col_group in required_columns]
+                    if list(df.columns[:3]) != correct_order:
+                        df = df[correct_order + list(df.columns[3:])]
+                    df['файл'] = file.name
+                    df['цена за кг.'] = round(df['цена'] / df['вес'], 1)
+                    if first_file:
+                        self.data = df
+                        first_file = False
+                    else:
+                        self.data = pd.concat([self.data, df], ignore_index=True)
                 except Exception as e:
                     print(f"Error processing file {file}: {e}")
         return self.data
